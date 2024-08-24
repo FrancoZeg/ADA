de41d0662ec178b04309d8618227065916c60a44
FrancoZeg <101823582+FrancoZeg@users.noreply.github.com>
2024-08-23 20:03:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 
-int gVect[100];
-int gnCount;
+int *gpVect = NULL,
+gnCount = 0,
+gnMax = 0;
+
+void Resize()
+{
+  const int delta = 10;
+  int *pTemp, i;
+  pTemp = new int[gnMax + delta]; 
+  for(i = 0 ; i < gnMax ; i++) 
+    pTemp[i] = gpVect[i]; 
+  delete [ ] gpVect; 
+  gpVect = pTemp; 
+  gnMax += delta; 
+}
 
 void Insert(int elem)
 {
-  if( gnCount < 100 )
-    gVect[gnCount++] = elem;
+  if( gnCount == gnMax )
+    Resize();
+  gpVect[gnCount++] = elem;
 }
 
 int main() { 
